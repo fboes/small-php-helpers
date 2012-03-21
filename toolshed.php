4771e23f3b6b32127b5d66fa7235d09ca2fc9808
Frank Boës <fboes@3960.org>
2012-03-21 13:51:49 +0100

    first commit

---
+++ toolshed.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Small set of general helpers (aka "The missing PHP functions")
+ *
+ * @author      Frank Bo"es <info@3960.org>
+ * @copyright   Creative Commons Attribution 3.0 Unported (CC BY 3.0)
+ */
+
+/**
+ * Echo with htmlspecialchars
+ *
+ * @param   string  $string for outputting
+ * @param   bool    $withBr convert line breaks into <br />. Optional,
+ *  defaults to false
+ */
+function _echo ($string, $withBr = FALSE)
+{
+    echo($withBr
+        ? htmlspecialchars($string)
+        : nl2br(htmlspecialchars($string))
+    );
+}
+
+/**
+ * Like printf, but $format will be quoted by htmlspecialchars.
+ * Arguments will _not_ be quoted.
+ *
+ * @param   string  $format
+ * @param   string  $args   and more args
+ */
+function _printf ()
+{
+    $args = func_get_args();
+    $format = array_shift($args);
+    _vprintf($format, $args);
+}
+
+/**
+ * Like sprintf, but $format will be quoted by htmlspecialchars
+ * Arguments will _not_ be quoted.
+ *
+ * @param   string  $format
+ * @param   string  $args   and more args
+ * @return  string
+ */
+function _sprintf ()
+{
+    $args = func_get_args();
+    $format = array_shift($args);
+    return _vsprintf($format, $args);
+}
+
+/**
+ * Like vprinft, but $format will be quoted by htmlspecialchars
+ * Arguments will _not_ be quoted.
+ *
+ * @param   string  $format
+ * @param   array   $args
+ */
+function _vprintf ($format, array $args)
+{
+    echo(_vsprintf($format, $args));
+}
+
+/**
+ * Like vsprintf, but $format will be quoted by htmlspecialchars
+ * Arguments will _not_ be quoted.
+ *
+ * @param   string  $format
+ * @param   array   $args
+ * @return  string
+ */
+function _vsprintf ($format, array $args)
+{
+    return vsprintf(htmlspecialchars($format), $args);
+}
+
+/**
+ * Checks if a scalar value is FALSE, without content or only full 
+ * whitespaces. 
+ * For non-scalar values will evaluate if value is empty().
+ *
+ * @param	mixed	$v	to test
+ * @return	bool	if $v is blank
+ */
+function is_blank (&$v)
+{
+    return !isset($v) || (is_scalar($v) ? (trim($v) === '') : empty($v));
+}
+
+/**
+ * Convert all characters into XML entities.
+ *
+ * @param   string  $string
+ * @return  string
+ */
+function htmlallchars ($string)
+{
+    $result = NULL;
+    for($i=0 ; $i<strlen($string) ; $i++) 
+    {
+        $result .= '&#'.ord($string[$i]).';';
+    }
+    return $result;
+}
+
+/**
+ * Set all internal switches for setting proper locale. Use 'locale -a'
+ * to determine available locales on your system.
+ *
+ * @param   string  $languageCode   according to ISO 639-1
+ * @param   string  $countryCode    according to ISO 3166
+ * @param   string  $charset    optional, defaults to 'utf-8'
+ */
+function set_locale ($languageCode, $countryCode, $charset = 'utf-8')
+{
+    $languageCode = strtolower($languageCode);
+    $countryCode  = strtoupper($countryCode);
+    $charset      = strtolower($charset);
+    
+    ini_set('default_charset', $charset);
+
+    $localeCode .= '.'.strtoupper(str_replace(' ','',$charset));
+    $categories = array(LC_COLLATE, LC_CTYPE, LC_TIME, LC_MESSAGES);
+    foreach ($categories as $c) 
+    {
+        setlocale($c, $localeCode);
+    }
+}
+
+if (!function_exists('_'))
+{
+    function _ ($string)
+    {
+        return $string;
+    }
+}
+?>
