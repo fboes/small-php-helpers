3106047bb1a32ae4e34cccc32d1c5aa1e0c405e7
Frank Boes <frank.boes@bluemars.net>
2012-08-23 11:06:31 +0200

    Added libs for simple output of structured variables

---
+++ HtmlEncode.class.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * @class HtmlEcnode
+ * Convert PHP construct to XML
+ */
+class HtmlEncode {
+  public $data;
+
+  /**
+   * [__construct description]
+   * @param mixed $data [description]
+   */
+  public function __construct ($data) {
+    $this->data = $data;
+  }
+
+  /**
+   * [output description]
+   * @return string [description]
+   */
+  public function output () {
+    $type = "";
+    if (is_array($this->data)) {
+      $type = ' array';
+    }
+    elseif (is_object($this->data)) {
+      $type = ' object"';
+    }
+    return '<div class="html-encode '.$type.'"">'.$this->outputNode($this->data).'</div>';
+  }
+
+  /**
+   * [outputNode description]
+   * @param  mixed $data [description]
+   * @return string       [description]
+   */
+  protected function outputNode ($data) {
+    $return = NULL;
+    if (is_bool($data)) {
+      $return = $data ? 1 : 0;
+    }
+    elseif (is_scalar($data)) {
+      $return = htmlspecialchars($data);
+    }
+    elseif (is_array($data)) {
+      $return .= '<ul>';
+      foreach ($data as $key => $value) {
+        $type = "";
+        if (is_array($value)) {
+          $type = ' class="array"';
+        }
+        elseif (is_object($value)) {
+          $type = ' class="object"';
+        }
+        elseif (is_integer($value)) {
+          $type = ' class="integer"';
+        }
+        elseif (is_bool($value)) {
+          $type = ' class="boolean"';
+        }
+        $return .= '<li '.$type.'><strong>'.htmlspecialchars($key).'</strong>: <span>'.$this->outputNode($value).'</span></li>';
+      }
+      $return .= '</ul>';
+    }
+    elseif (is_object($data)) {
+      $objData = get_object_vars($data);
+      $return .= $this->outputNode($objData);
+    }
+    return $return;
+  }
+
+  /**
+   * Static constructor with output. Converting PHP construct to XML string
+   * @param  mixed $data [description]
+   * @return string       [description]
+   */
+  public static function encode ($data) {
+    $xml = new self($data);
+    return $xml->output();
+  }
+
+}
---
+++ Messages.class.php
@@ -0,0 +1,92 @@
+<?php
+
+class Messages {
+  public $httpStatusCode = 200;
+  public $messages = array();
+
+  /**
+   * [__construct description]
+   */
+  public function __construct () {
+    $this->messages = array();
+    $this->httpStatusCode = 200;
+  }
+
+  /**
+   * [addSuccessMessage description]
+   * @param string  $message [description]
+   * @param boolean $success [description]
+   */
+  public function addSuccessMessage ($message, $success = TRUE) {
+    $this->addMessage($message);
+    $this->httpStatusCode = max($this->httpStatusCode, $success ? 200 : 400);
+    return $this;
+  }
+
+  /**
+   * [addhttpStatusCode description]
+   * @param string  $message    [description]
+   * @param integer $httpStatusCode [description]
+   */
+  public function addHttpStatusCode ($message, $httpStatusCode = 200) {
+    $this->addMessage($message);
+    $this->httpStatusCode = max($this->httpStatusCode, (int)$httpStatusCode);
+    return $this;
+  }
+
+  /**
+   * Add error message depending on assertion
+   * @param bool $assert      [description]
+   * @param string $message     [description]
+   * @param string $messageFail [description]
+   */
+  public function addMessageOnAssert ($assert, $message, $messageFail = '') {
+    if (empty($messageFail)) {
+      $messageFail = str_replace(
+        array(' success',' succeeded', ' exists', ' is', ' has'),
+        array(' fail',' failed', ' does not exist', ' is not', ' has not'),
+        $message
+      );
+    }
+    $this->addSuccessMessage($assert ? $message : $messageFail, $assert);
+    return $this;
+  }
+
+  /**
+   * Adds a single message to $this->messages
+   * @param string $message [description]
+   */
+  public function addMessage ($message) {
+    $this->messages[] = new Message($message);
+  }
+
+  /**
+   * [buildhttpStatusCode description]
+   * @return string [description]
+   */
+  public function buildhttpStatusCode () {
+    $message = !empty($this->messages) 
+      ? end($this->messages)->message
+      : ''
+    ;
+    return 'HTTP/1.1 ' . $this->httpStatusCode . ' ' . $message;
+  }
+
+  /**
+   * [isSuccess description]
+   * @return boolean [description]
+   */
+  public function isSuccess () {
+    return $this->httpStatusCode < 400;
+  }
+}
+
+class Message {
+  public $ts;
+  public $message;
+
+  public function __construct ($message) {
+    $this->ts = time();
+    $this->message = (string)$message;
+  }
+}
