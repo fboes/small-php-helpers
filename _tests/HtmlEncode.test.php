039e49581abae96e246eb6421c76304099a0cd2e
Frank Boës <fboes@3960.org>
2012-11-04 14:06:01 +0100

    Improved tests checking validity of output

--- _tests/HtmlEncode.test.php
+++ _tests/HtmlEncode.test.php
@@ -9,6 +9,19 @@ class HtmlEncodeTest extends Tester {
 		$this->assertClassHasAttribute('data','HtmlEncode');
 	}
 
+	public function dataHtml () {
+		return array (
+			array('a'),
+			array(array(1,2,3)),
+			array((object)array('a' => 'b')),
+		);
+	}
+
+	public function testHtml ($a) {
+		$obj = new HtmlEncode($a);
+		$this->assertValidHtml($obj->output());
+
+	}
 }
 
 HtmlEncodeTest::doTest();
--- _tests/XmlEncode.test.php
+++ _tests/XmlEncode.test.php
@@ -9,6 +9,19 @@ class XmlEncodeTest extends Tester {
 		$this->assertClassHasAttribute('data','XmlEncode');
 	}
 
+	public function dataXml () {
+		return array (
+			array('a'),
+			array(array(1,2,3)),
+			array((object)array('a' => 'b')),
+		);
+	}
+
+	public function testXml ($a) {
+		$obj = new XmlEncode($a);
+		$this->assertValidXml($obj->output());
+
+	}
 }
 
 XmlEncodeTest::doTest();
