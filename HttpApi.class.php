10ee0afcdd9c55282ad5ec978fc19ee908ce71e3
Frank Boes <frank.boes@bluemars.net>
2013-06-26 10:03:16 +0200

    Adding memoization, improved tests for HttpApi

--- HttpApi.class.php
+++ HttpApi.class.php
@@ -9,8 +9,12 @@ class HttpApi {
 	protected $lastUrl;
 	protected $lastPostFields;
 
-	public $lastReply;
+	protected $memoizationTable = array();
+	protected $memoizationExpire = 5;
+
+	public $lastMemoizationKey;
 	public $lastHttpStatusCode;
+	public $lastReply;
 
 	const RETURN_TYPE_PLAIN = 'PLAIN';
 	const RETURN_TYPE_JSON  = 'JSON';
@@ -91,73 +95,85 @@ class HttpApi {
 	 * @return mixed  see $this->convertReply
 	 */
 	public function doRequest (array $data = NULL, $url = NULL, $type = self::HTTP_GET) {
-		$url  = $this->baseUrl . $url;
+		$url  = $this->baseUrl . (string)$url;
+		$type = (string)$type;
 		$data = http_build_query($data);
+		$this->lastMemoizationKey = $type . ' ' .$url . '?' . $data;
 
 		if (empty($url)) {
 			throw new Exception('Empty URL');
 		}
 
-		$ch = curl_init();
-
-		$curlOptions = array();
-		$curlOptions[CURLOPT_URL] = $url;
-
-		switch ($type) {
-			case self::HTTP_POST:
-				$curlOptions[CURLOPT_POST] = TRUE;
-				if (!empty($data)) {
-					$curlOptions[CURLOPT_POSTFIELDS] = $data;
-				}
-				break;
-			case self::HTTP_PUT:
-				$curlOptions[CURLOPT_POST] = TRUE;
-				$curlOptions[CURLOPT_CUSTOMREQUEST] = $type;
-				if (!empty($data)) {
-					$curlOptions[CURLOPT_POSTFIELDS] = $data;
-				}
-				break;
-			case self::HTTP_DELETE:
-				$curlOptions[CURLOPT_POST] = TRUE;
-				$curlOptions[CURLOPT_CUSTOMREQUEST] = $type;
-				if (!empty($data)) {
-					$curlOptions[CURLOPT_POSTFIELDS] = $data;
-				}
-				break;
-			default:
-				if (!empty($data)) {
-					$curlOptions[CURLOPT_URL] .= '?'.$data;
-				}
-				break;
-		}
-		if (!empty($this->httpUsername) || !empty($this->httpPassword)) {
-			$curlOptions[CURLOPT_HTTPAUTH] = CURLAUTH_BASIC;
-			$curlOptions[CURLOPT_USERPWD]  = $this->httpUsername . ':' . $this->httpPassword;
-		}
-
-		$curlOptions[CURLOPT_HEADER] = 0;
-		$curlOptions[CURLOPT_RETURNTRANSFER] = TRUE;
-		$curlOptions[CURLOPT_CONNECTTIMEOUT] = 15;
-
-		curl_setopt_array($ch, $curlOptions);
-		$this->lastUrl        = $curlOptions[CURLOPT_URL];
-		$this->lastPostFields = !empty($curlOptions[CURLOPT_POSTFIELDS])
-			? $curlOptions[CURLOPT_POSTFIELDS]
-			: NULL
-		;
-		$reply = curl_exec($ch);
-
-		$this->lastReply = NULL;
-		if(curl_errno($ch)) {
-			throw new Exception('Curl Error: '.curl_error($ch));
+		$memoization = $this->memoizationGet($this->lastMemoizationKey);
+		if ($this->memoizationExpire && !empty($memoization)) {
+			$this->lastReply = $memoization;
+			$this->lastHttpStatusCode = 200;
 		}
 		else {
-			$this->lastHttpStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE );
-			if (!empty($reply)) {
-				$this->lastReply = $this->convertReply($reply);
+			$ch = curl_init();
+
+			$curlOptions = array();
+			$curlOptions[CURLOPT_URL] = $url;
+
+			switch ($type) {
+				case self::HTTP_POST:
+					$curlOptions[CURLOPT_POST] = TRUE;
+					if (!empty($data)) {
+						$curlOptions[CURLOPT_POSTFIELDS] = $data;
+					}
+					break;
+				case self::HTTP_PUT:
+					$curlOptions[CURLOPT_POST] = TRUE;
+					$curlOptions[CURLOPT_CUSTOMREQUEST] = $type;
+					if (!empty($data)) {
+						$curlOptions[CURLOPT_POSTFIELDS] = $data;
+					}
+					break;
+				case self::HTTP_DELETE:
+					$curlOptions[CURLOPT_POST] = TRUE;
+					$curlOptions[CURLOPT_CUSTOMREQUEST] = $type;
+					if (!empty($data)) {
+						$curlOptions[CURLOPT_POSTFIELDS] = $data;
+					}
+					break;
+				default:
+					if (!empty($data)) {
+						$curlOptions[CURLOPT_URL] .= '?'.$data;
+					}
+					break;
 			}
+			if (!empty($this->httpUsername) || !empty($this->httpPassword)) {
+				$curlOptions[CURLOPT_HTTPAUTH] = CURLAUTH_BASIC;
+				$curlOptions[CURLOPT_USERPWD]  = $this->httpUsername . ':' . $this->httpPassword;
+			}
+
+			$curlOptions[CURLOPT_HEADER] = 0;
+			$curlOptions[CURLOPT_RETURNTRANSFER] = TRUE;
+			$curlOptions[CURLOPT_CONNECTTIMEOUT] = 15;
+
+			curl_setopt_array($ch, $curlOptions);
+			$this->lastUrl        = $curlOptions[CURLOPT_URL];
+			$this->lastPostFields = !empty($curlOptions[CURLOPT_POSTFIELDS])
+				? $curlOptions[CURLOPT_POSTFIELDS]
+				: NULL
+			;
+			$reply = curl_exec($ch);
+
+			$this->lastReply = NULL;
+			if(curl_errno($ch)) {
+				throw new Exception('Curl Error: '.curl_error($ch));
+			}
+			else {
+				$this->lastHttpStatusCode = (int)curl_getinfo($ch, CURLINFO_HTTP_CODE );
+				if (!empty($reply)) {
+					$this->lastReply = $this->convertReply($reply);
+				}
+				if ($this->memoizationExpire){
+					$this->memoizationSet($this->lastMemoizationKey, $this->lastReply, $this->memoizationExpire);
+				}
+			}
+			curl_close($ch);
 		}
-		curl_close($ch);
 		return $this->lastReply;
 	}
 
@@ -210,5 +226,66 @@ class HttpApi {
 				return $data;
 				break;
 		}
+	}
+
+	/**
+	 * Get data for $key
+	 * @param  string $key The key to fetch.
+	 * @return mixed       [description]
+	 */
+	protected function memoizationGet ($key) {
+		$key = (string)$key;
+		if (!empty($this->memoizationTable[$key])) {
+			$memo = $this->memoizationTable[$key];
+			if (!empty($memo->expire) && $memo->expire < time()) {
+				$this->memoizationDelete($key);
+				return NULL;
+			}
+			else {
+				return $memo->var;
+			}
+		}
+		return NULL;
+	}
+
+	/**
+	 * [memoizationSet description]
+	 * @param  string $key    The key that will be associated with the item.
+	 * @param  mixed  $data   The variable to store.
+	 * @param  int    $expire Expiration time of the item. If it's equal to zero, the item will never expire. You can also use Unix timestamp or a number of seconds starting from current time, but in the latter case the number of seconds may not exceed 2592000 (30 days).
+	 * @return bool           [description]
+	 */
+	protected function memoizationSet ($key, $var, $expire = 0) {
+		if (!empty($expire) && $expire < 2592000) {
+			$expire += time();
+		}
+		$this->memoizationTable[(string)$key] = (object)array(
+			'expire' => $expire,
+			'var'    => $var
+		);
+		return TRUE;
+	}
+
+	/**
+	 * Delete specific key
+	 * @param  string $key [description]
+	 * @return bool        [description]
+	 */
+	public function memoizationDelete ($key) {
+		$key = (string)$key;
+		if (!empty($key) && !empty($this->memoizationTable[$key])) {
+			unset($this->memoizationTable[$key]);
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+	/**
+	 * [memoizationFlush description]
+	 * @return bool [description]
+	 */
+	public function memoizationFlush () {
+		$this->memoizationTable = array();
+		return TRUE;
 	}
 }
--- _tests/HttpApi.test.php
+++ _tests/HttpApi.test.php
@@ -4,15 +4,37 @@ require('../Tester.class.php');
 require('../HttpApi.class.php');
 
 class HttpApiTest extends Tester {
-	public function testObjectInvocation () {
+	public function testSimple () {
 		$baseUrl = 'http://3960.org/';
+
+		$this->assertMethodExists('get',                'HttpApi');
+		$this->assertMethodExists('post',               'HttpApi');
+		$this->assertMethodExists('put',                'HttpApi');
+		$this->assertMethodExists('delete',             'HttpApi');
+		$this->assertMethodExists('doRequest',          'HttpApi');
+		$this->assertMethodExists('setHttpCredentials', 'HttpApi');
 
 		$api = new HttpApi($baseUrl, HttpApi::RETURN_TYPE_HTML);
 		$this->assertTrue(is_object($api));
-		$this->outputLine($api);
 
+	}
+
+	public function testObjectInvocation () {
+		$baseUrl = 'http://3960.org/';
+		$api = new HttpApi($baseUrl, HttpApi::RETURN_TYPE_HTML);
 		$result = $api->get(array('a' => 'b'));
 		$this->assertTrue(!empty($result));
+	}
+
+	public function testXmlConversion () {
+		$baseUrl = 'http://3960.org/';
+
+		$api = new HttpApi($baseUrl, HttpApi::RETURN_TYPE_XML);
+
+		$result = $api->get(array(), 'index.xml');
+		$this->assertTrue(!empty($result));
+		$this->assertEquals(get_class($result), 'SimpleXMLElement');
+		$this->outputLine($api);
 	}
 }
 
